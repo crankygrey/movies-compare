c980456f5b42b2567640efb5a6b0eeb30beee632
chan myae <30973264+crankygrey@users.noreply.github.com>
2020-03-08 04:29:07 +0630

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const autocompleteConfig = {
 		return movie.Title;
 	},
 	async fetchData(searchTerm) {
-		const response = await axios.get('http://www.omdbapi.com/', {
+		const response = await axios.get('https://www.omdbapi.com/', {
 			params: {
 				apikey: '3352a493',
 				s: searchTerm
